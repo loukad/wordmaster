16591dd2e3540e1d01c273806fb8096c8b7df2e8
Louka Dlagnekov <loukad@gmail.com>
2022-03-28 14:10:20 -1000

    Support for multiple dictionaries

--- app/wm.py
+++ app/wm.py
@@ -1,11 +1,6 @@
-import os
-import sys
-import sqlite3
-import flask
-
 from datetime import datetime
 
-from flask import Flask, g, render_template, request, url_for
+from flask import Flask, g, redirect, render_template, request, url_for
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.sql.expression import func
 
@@ -22,25 +17,53 @@ class Word(db.Model):
     definition = db.Column(db.String, nullable=False)
     added = db.Column(db.DateTime, default=datetime.utcnow)
 
+    dictionary_id = db.Column(db.Integer, db.ForeignKey('dictionary.id'))
+
+class Dictionary(db.Model):
+    __tablename__ = 'dictionary'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, unique=True, nullable=False)
+
+    words = db.relationship('Word', backref='dictionary')
+
+def requires_dictionary(func):
+    ''' Decorator for ensuring dictionary is loaded. '''
+    def ensure_exists(name, **moreargs):
+        did = db.session.query(Dictionary).filter_by(name=name).first()
+        if did is None:
+            return redirect(url_for('list'))
+        return func(did, **moreargs)
+    ensure_exists.__name__ = func.__name__
+    return ensure_exists
+
+
 @app.after_request
 def add_header(response):
     response.headers['Cache-Control'] = 'max-age=0'
     return response
 
-def word_count():
-    return db.session.query(Word).count()
+def word_selector(did, **selector):
+    return db.session.query(Word).filter_by(dictionary_id=did, **selector)
 
-def get_definition(word):
-    return db.session.query(Word).filter(Word.word == word).first()
+def word_count(did):
+    return word_selector(did).count()
 
-def insert_definition(word, definition):
-    db.session.add(Word(word=word, definition=definition))
+def get_definition(did, word):
+    return word_selector(did, word=word).first()
+
+def insert_definition(did, word, definition):
+    db.session.add(Word(dictionary_id=did, word=word, definition=definition))
     db.session.commit()
 
 @app.route('/')
-@app.route('/add')
-def add_word():
+def list():
+    dicts = db.session.query(Dictionary).all()
+    return render_template('list.html', dictionaries=dicts)
 
+@app.route('/<name>/add')
+@requires_dictionary
+def add_word(d):
     msg_kind = 'info'
     message = ''
 
@@ -50,7 +73,7 @@ def add_word():
 
     if word:
         # Is the word already in the dictionary?
-        previous = get_definition(word)
+        previous = get_definition(d.id, word)
         if previous is not None:
             del_url = url_for('delete_word', word=word)
             msg_kind = 'error'
@@ -61,31 +84,39 @@ def add_word():
             insert_definition(word, definition)
             message = f'Added <em>{word}</em> : {definition} to dictionary'
 
-    return render_template('add.html', word_count=word_count(),
-                                       msg_kind=msg_kind,
-                                       message=message)
+    params = dict(name=d.name, word_count=word_count(d.id), msg_kind=msg_kind,
+                  message=message)
+    return render_template('add.html', **params)
 
-@app.route("/view")
-def view():
-    words = db.session.query(Word).order_by(Word.added.desc()).limit(200).all()
-    return render_template('view.html', word_count=word_count(), words=words)
+def template_selector(template, d, words):
+    params = dict(word_count=word_count(d.id), words=words, name=d.name)
+    return render_template(template, **params)
 
-@app.route("/view/all")
-def view_all():
+@app.route("/<name>/view")
+@requires_dictionary
+def view(d):
+    words = word_selector(d.id).order_by(Word.added.desc()).limit(200).all()
+    return template_selector('view.html', d, words)
+
+@app.route("/<name>/view/all")
+@requires_dictionary
+def view_all(d):
     words = db.session.query(Word).order_by(Word.added.desc()).all()
-    return render_template('view.html', word_count=word_count(), words=words)
+    return template_selector('view.html', d, words)
 
-@app.route("/rand")
-def random():
-    words = db.session.query(Word).order_by(func.random()).limit(1).all()
-    return render_template('rand.html', word_count=word_count(), words=words)
+@app.route("/<name>/rand")
+@requires_dictionary
+def random(d):
+    words = word_selector(d.id).order_by(func.random()).limit(1).all()
+    return template_selector('rand.html', d, words)
 
-@app.route("/delete/<word>")
-def delete_word(word):
+@app.route("/<name>/delete/<word>")
+@requires_dictionary
+def delete_word(d, word):
     msg_kind = 'info'
     message = f'Deleted word <em>{word}</em>'
 
-    found = db.session.query(Word).filter(Word.word == word).one()
+    found = word_selector(d.id, word=word).one()
     if found is None:
         msg_kind = 'error'
         message = f'Could not find word {word} to delete'
@@ -93,5 +124,6 @@ def delete_word(word):
         db.session.delete(found)
         db.session.commit()
 
-    return render_template('add.html', msg_kind=msg_kind, message=message)
+    params = dict(name=d.name, msg_kind=msg_kind, message=message)
+    return render_template('add.html', **params)
 
